e75841ae99a487a6584962455a69492157c85943
Priyanshu Gupta <gpriyanshu995@gmail.com>
2025-11-15 18:29:56 +0530

    added whatsapp

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import WhatsAppWidget from "./components/WhatsAppWidget";
 import "./globals.css";
 
 export const metadata = {
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
       >
+            <WhatsAppWidget/>
         {children}
       </body>
     </html>
